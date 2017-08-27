de13cd1a6c6a1f145371598488415bf3c34bd210
Aaron Garcia <tigreaaron@hotmail.com>
2017-08-26 19:50:45 -0500

    Coordenadas class, ubicacions txt

---
+++ bus/Coordenada.swift
@@ -0,0 +1,30 @@
+//
+//  Coordenada.swift
+//  bus
+//
+//  Created by A4RON on 8/26/17.
+//  Copyright © 2017 A4RON. All rights reserved.
+//
+
+import Foundation
+
+class Coordenada {
+    init() {
+        
+    }
+    var lati = 0.0;
+    var longi = 0.0;
+    
+    init(lati: Double, longi: Double) {
+        self.lati = lati
+        self.longi = longi
+    }
+    
+    func getLati() -> Double {
+        return self.lati
+    }
+    
+    func getLongi() -> Double {
+        return self.longi
+    }
+}
